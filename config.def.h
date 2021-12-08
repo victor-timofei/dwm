aa5b6090f83ea55c145d371adfe7ee3a20a94e02
Victor Timofei <victor@vtimothy.com>
2021-12-08 10:55:19 +0200

    Add poweroff & restart commands

--- config.def.h
+++ config.def.h
@@ -105,10 +105,14 @@ static const char *dmenucmd[] = { "dmenu_run",
 	NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *togglelang[]  = { "/home/vtimofei/scripts/toggle-language", NULL };
+static const char *poweroff[]  = { "/home/vtimofei/scripts/poweroff", NULL };
+static const char *reboot[]  = { "/home/vtimofei/scripts/reboot", NULL };
 
 static Key keys[] = {
 	/* modifier                     key                      function        argument */
 	{ MODKEY,                       XK_p,                    spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_p,                    spawn,          {.v = poweroff } },
+	{ MODKEY|ShiftMask,             XK_r,                    spawn,          {.v = reboot } },
 	{ MODKEY|ShiftMask,             XK_Return,               spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,                    togglebar,      {0} },
 	{ MODKEY,                       XK_j,                    focusstack,     {.i = +1 } },
